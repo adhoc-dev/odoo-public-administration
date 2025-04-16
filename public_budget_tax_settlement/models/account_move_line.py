96b00f0c0a04a4ea5146c69a347b00643b5cfe06
Ignacio Ramirez <iga@adhoc.com.ar>
2025-04-16 14:48:13 -0300

    [MIG] public_budget_tax_settlement : Fix views and change relation to compute

--- public_budget_tax_settlement/models/account_move_line.py
+++ public_budget_tax_settlement/models/account_move_line.py
@@ -5,5 +5,11 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     payment_methods = fields.Char(
-        related='payment_id.payment_group_id.payment_methods',
+        # related='payment_ids.payment_method',
+        compute="_compute_payment_methods",
     )
+
+
+    def _compute_payment_methods(self):
+        for rec in self:
+            rec.payment_methods = ', '.join(rec.payment_ids.mapped('payment_method'))
